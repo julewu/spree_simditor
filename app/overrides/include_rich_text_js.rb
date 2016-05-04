97d0277f79bbe8e6465d2d5abbed56cf10114067
LiJunjie <holysoros@gmail.com>
2016-05-04 14:25:59 +0800

    Only try to initialize simditor in edit page of products

--- app/overrides/include_rich_text_js.rb
+++ app/overrides/include_rich_text_js.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
+  virtual_path: 'spree/admin/products/_form',
   name: 'include_rich_text_js',
-  insert_bottom: '[data-hook="admin_footer_scripts"]',
+  insert_bottom: '[data-hook="admin_product_form_fields"]',
   text: '<%= render partial: "shared/simditor" %>'
 )
